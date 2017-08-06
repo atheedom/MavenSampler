400916f9794b86b8fab206b42beefbdb874704b2
Alex Theedom <alex.theedom@gmail.com>
2017-08-06 18:34:15 +0100

    Tidy application

---
+++ src/main/java/com/readlearncode/Application.java
@@ -0,0 +1,13 @@
+package com.readlearncode;
+
+/**
+ * Source code github.com/atheedom
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
